23f1c70183c43a7c725d63b565eae11fcf246601
Duc Nguyen <minhduc140583@gmail.com>
2021-06-13 20:35:39 +0100

    Init project

---
+++ src/init.ts
@@ -0,0 +1,11 @@
+import {Db} from 'mongodb';
+import {ApplicationContext} from './context';
+import {UserController} from './controllers/UserController';
+import {MongoUserService} from './services/mongo/MongoUserService';
+
+export function createContext(db: Db): ApplicationContext {
+    const userService = new MongoUserService(db);
+    const userController = new UserController(userService);
+    const ctx: ApplicationContext = {userController};
+    return ctx;
+}
