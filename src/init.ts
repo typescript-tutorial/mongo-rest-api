f11829d720ffba4adc20804f255859aca0ef021a
minhduc140583 <minhduc140583@gmail.com>
2021-08-15 15:19:39 +0700

    Upgrade version

--- src/init.ts
+++ src/init.ts
@@ -4,8 +4,8 @@ import {UserController} from './controllers/UserController';
 import {MongoUserService} from './services/mongo/MongoUserService';
 
 export function createContext(db: Db): ApplicationContext {
-    const userService = new MongoUserService(db);
-    const userController = new UserController(userService);
-    const ctx: ApplicationContext = {userController};
-    return ctx;
+  const userService = new MongoUserService(db);
+  const userController = new UserController(userService);
+  const ctx: ApplicationContext = { userController };
+  return ctx;
 }
